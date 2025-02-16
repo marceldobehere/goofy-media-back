22b3cd3d5242b52f4a45cf7ee02c3550643b50c0
Masl <marcel.z2006@gmail.com>
2025-02-16 06:24:57 +0100

    auth and lock middleware

---
+++ routes/asyncLock.js
@@ -0,0 +1,77 @@
+const logLock = false;
+
+class AsyncLock {
+    constructor () {
+        this.promiseArr = [];
+        this.resolveArr = [];
+    }
+
+    disable ()
+    {
+        if (this.resolveArr.length > 0)
+        {
+            if (logLock)
+                console.log("Disabling lock");
+
+            this.promiseArr.shift();
+            this.resolveArr.shift()();
+        }
+        else
+            alert("Invalid lock disable")
+    }
+
+    async enable ()
+    {
+        if (logLock)
+            console.log("Enabling lock");
+
+        let tempPromises = [];
+        for (let prom of this.promiseArr)
+            tempPromises.push(prom);
+        let bigPromise = Promise.all(tempPromises);
+
+        let resolve;
+        let promise = new Promise(r => resolve = r);
+        this.promiseArr.push(promise);
+        this.resolveArr.push(resolve);
+
+        await bigPromise;
+    }
+
+    isLocked()
+    {
+        return this.resolveArr.length > 0;
+    }
+
+    reset()
+    {
+        this.promiseArr = [];
+        this.resolveArr = [];
+    }
+
+    async tryEnable ()
+    {
+        if (logLock)
+            console.log("Trying to enable lock");
+
+        if (this.resolveArr.length > 0)
+            return false;
+
+        await this.enable();
+        return true;
+    }
+
+    async do(func) {
+        await this.enable();
+        try {
+            let res = await func();
+            this.disable();
+            return res;
+        } catch (e) {
+            this.disable();
+            console.error("Error: ", e);
+        }
+    }
+}
+
+module.exports = AsyncLock;
---
+++ routes/authValidation.js
@@ -0,0 +1,125 @@
+global.window = this
+const JSEncrypt = require('jsencrypt');
+const CryptoJS = require('crypto-js');
+const AsyncLock = require('./asyncLock');
+
+
+// check if id exists with public key
+let idList = [];
+const msExtra = 1000 * 30;
+async function checkId(publicKey, id, validUntil) {
+    // console.log(" > Checking id: ", id, publicKey, validUntil)
+    // console.log(" > List: ", idList)
+    const now = Date.now();
+    if (validUntil < now || validUntil > now + msExtra)
+        return false;
+
+    // clear list of old entries
+    idList = idList.filter(x => x.validUntil > now && x.validUntil < now + msExtra);
+
+    // check if id already exists
+    let found = idList.filter(x => x.id === id && x.publicKey === publicKey).length > 0;
+    if (found)
+        return false;
+
+    // add id to list
+    idList.push({id, publicKey, validUntil});
+    return true;
+}
+
+async function getHashFromObj(obj)
+{
+    let hash = CryptoJS.SHA256(JSON.stringify(obj)).toString(CryptoJS.enc.Base64);
+    return hash;
+}
+
+async function verifyRequest(body, signature, id, validUntil, publicKey) {
+    let hash = await getHashFromObj({body, id, validUntil});
+    // console.log(" > Obj: ", {body, id, validUntil})
+    // console.log(" > Hash: ", hash)
+
+    const encrypt = new JSEncrypt();
+    encrypt.setPublicKey(publicKey);
+
+    let verified = encrypt.verify(hash, signature, CryptoJS.SHA256);
+
+    // check if id already exists
+
+    // check if validUntil is in the future
+    if (validUntil < Date.now())
+        verified = false;
+
+    // but check if its not too much in the future
+    if (validUntil > Date.now() + msExtra)
+        verified = false;
+
+    // check id list
+    let idCheck = await checkId(publicKey, id, validUntil);
+    if (!idCheck)
+        verified = false;
+
+    return verified;
+}
+
+function parseHeaders(headers) {
+    let id = headers['x-goofy-id']
+    let signature = headers['x-goofy-signature']
+    let validUntil = headers['x-goofy-valid-until']
+    let publicKey = headers['x-goofy-public-key']
+
+    if (id === undefined || signature === undefined || validUntil === undefined || publicKey === undefined)
+        return null;
+
+    try {
+        id = parseInt(id)
+        validUntil = parseInt(validUntil)
+    } catch (e) {
+        return null;
+    }
+
+    return {id, signature, validUntil, publicKey: decodeURIComponent(publicKey)};
+}
+
+const authLocks = new Map();
+
+const authMiddleware = async (req, res, next) => {
+    let {id, signature, validUntil, publicKey} = parseHeaders(req.headers);
+    if (id === null)
+        return res.status(400).send("Bad request");
+
+    // console.log("> Parsed: ")
+    // console.log(" > id: ", id)
+    // console.log(" > signature: ", signature)
+    // console.log(" > validUntil: ", validUntil)
+    // console.log(" > publicKey: ", publicKey)
+    let verified = await verifyRequest(req.body, signature, id, validUntil, publicKey);
+    if (!verified)
+        return res.status(401).send("Unauthorized");
+
+    req.publicKey = publicKey;
+
+
+    // locks for each url
+    let idx = `${req.url}_${req.method}`;
+    if (!authLocks.has(idx))
+        authLocks.set(idx, new AsyncLock());
+    const lock = authLocks.get(idx);
+    // console.log(" > Got lock: ", url, lock)
+
+    // await lock.do(next);
+
+    res.lock = async (func) => {await lock.do(func)};
+    await next();
+};
+
+const lockMiddleware = async (req, res, next) => {
+    let idx = `${req.url}_${req.method}`;
+    if (!authLocks.has(idx))
+        authLocks.set(idx, new AsyncLock());
+    const lock = authLocks.get(idx);
+    res.lock = async (func) => {await lock.do(func)};
+    await next();
+};
+
+
+module.exports = {authMiddleware, lockMiddleware};
