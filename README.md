59208929cad61697296cb7302650df2228a524bf
Masl <marcel.z2006@gmail.com>
2025-02-18 22:54:00 +0100

    yes

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Currently very WIP.
 
 (More details will follow with time)
 
+
+
+Requires a `.env` file with the following variables:
+```
+DB_USER="..."
+DB_PASS="..."
+DB_URL="mongodb+srv://..."
+```
--- services/db/db_internal.js
+++ services/db/db_internal.js
@@ -14,26 +14,3 @@ const prom = new Promise(async (resolve, reject) => {
 });
 
 module.exports = prom;
-
-
-/*
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-let connectionPromiseResolve;
-const connectionPromise = new Promise((resolve) => {
-    connectionPromiseResolve = resolve;
-});
-async function run() {
-    try {
-        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-        await mongoose.connect(uri, clientOptions);
-        await mongoose.connection.db.admin().command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-        connectionPromiseResolve();
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await mongoose.disconnect();
-    }
-}
-run().catch(console.dir);
-
-*/
