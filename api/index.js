8b3197cdcb574a697eed8dc42225e3aded72e0e3
Masl <marcel.z2006@gmail.com>
2025-02-22 23:55:19 +0100

    wa

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ if (SSL && !fs.existsSync(__dirname + "/data/ssl"))
     console.log("openssl genrsa -out key.pem");
     console.log("openssl req -new -key key.pem -out csr.pem");
     console.log("openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem");
-    return;
+    process.exit(1);
 }
 
 var app = require('../app');
