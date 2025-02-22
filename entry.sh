47296902b9d97ab5c4f7dc6d589f15987a49d70a
marceldobehere2 <marvelzietek@gmail.com>
2025-02-22 23:16:53 +0100

    wa

---
+++ entry.sh
@@ -0,0 +1,7 @@
+mkdir data/ssl -p
+
+# Change this to your domain
+cp /etc/letsencrypt/live/media.marceldobehere.com/fullchain.pem data/ssl/cert.pem
+cp /etc/letsencrypt/live/media.marceldobehere.com/privkey.pem data/ssl/key.pem
+
+exec node api/index.js SSL
