9cc50c80b42d7a3c38dd488e0bc09aee82124126
Masl <marcel.z2006@gmail.com>
2025-02-22 23:46:15 +0100

    schwubdiwup

--- api/index.js
+++ api/index.js
@@ -1,90 +1,45 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
-/**
- * Module dependencies.
- */
+let SSL = false;
+if (process.argv.length > 2 &&
+    process.argv[2].toLowerCase() === 'ssl')
+    SSL = true;
+
+if (!fs.existsSync(__dirname + "/data"))
+{
+    fs.mkdirSync(__dirname + "/data");
+}
+
+if (SSL && !fs.existsSync(__dirname + "/data/ssl"))
+{
+    console.log("SSL FOLDER DOESNT EXIST");
+    console.log();
+    console.log("To create the ssl keys, open a terminal in the data folder and run the following commands:");
+    console.log("mkdir ssl");
+    console.log("cd ssl");
+    console.log("openssl genrsa -out key.pem");
+    console.log("openssl req -new -key key.pem -out csr.pem");
+    console.log("openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem");
+    return;
+}
 
 var app = require('../app');
-var debug = console.debug;
-var http = require('http');
 
-/**
- * Get port from environment and store in Express.
- */
+let server;
+if (!SSL)
+    server = http.createServer(app);
+else
+    server = https.createServer(
+        {
+            key: fs.readFileSync(__dirname + "/data/ssl/key.pem"),
+            cert: fs.readFileSync(__dirname + "/data/ssl/cert.pem"),
+        },
+        app);
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.on('error', onError);
-server.on('listening', onListening);
-server.listen(port);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+let port = SSL ? 443 : 3000;
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
