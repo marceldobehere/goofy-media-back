1136dfbae3bb8e7c1ddcaa65d4c7dcc14e0ff9c8
Masl <marcel.z2006@gmail.com>
2025-02-23 15:49:20 +0100

    redirect to static frontend

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // redirect to https://marceldobehere.github.io/goofy-media-front/
+    res.redirect('https://marceldobehere.github.io/goofy-media-front/');
 });
 
 module.exports = router;
