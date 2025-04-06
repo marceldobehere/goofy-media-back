2e61452f3c04b79c789f5ad25d671c76e451955e
Masl <marcel.z2006@gmail.com>
2025-04-06 05:18:06 +0200

    yes 3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import adminRouter from './routes/admin/adminRoutes.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: '15mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
