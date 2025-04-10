97747b1d6c30eab32010700207627e31d8f22c0c
Masl <marcel.z2006@gmail.com>
2025-04-10 19:42:48 +0200

    user redirect

--- routes/smol/smol.js
+++ routes/smol/smol.js
@@ -9,4 +9,12 @@ router.get("/post/:uuid", async (req, res) => {
     res.redirect(`${process.env.CLIENT_URL}/user/post?uuid=${encodeURIComponent(uuid)}&serverId=${encodeURIComponent(process.env.SERVER_URL)}`)
 });
 
+router.get("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    if (!userId)
+        return res.status(400).send('Missing userId');
+
+    res.redirect(`${process.env.CLIENT_URL}/user/profile?userId=${encodeURIComponent(userId)}&serverId=${encodeURIComponent(process.env.SERVER_URL)}`)
+});
+
 export default router;
