76ca16c72eb9bfcbe78f41c666ddb983a9b64119
Masl <marcel.z2006@gmail.com>
2025-04-10 13:56:32 +0200

    compression

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import './services/loadConf.js'
 import express from 'express';
 import cors from 'cors';
+import compression from 'compression';
 
 
 import indexRouter from './routes/index.js';
@@ -13,6 +14,7 @@ const app = express();
 app.use(express.json({limit: '15mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(compression());
 
 app.use('/', indexRouter);
 app.use('/guest', guestRouter);
