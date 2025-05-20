6775905e6708eaaa97569caaf0c449bc46dcfed1
Masl <marcel.z2006@gmail.com>
2025-05-20 18:00:52 +0200

    caching for cors lol

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = http2Express(express);
 
 app.use(express.json({limit: '15mb'}));
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({maxAge: 86400}));
 app.use(compression());
 app.use(fileUpload({
     // useTempFiles: true,
